e2834e7b00a9c730c4e9cd78ffe710284175144c
NasirHussain GitHub <nh246nasir@gmail.com>
2023-10-05 23:02:05 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route,Routes } from "react-router-dom";
+import Product from "./Product";
+import Cart from "./Cart";
+import Login from "./Login";
+import SignUp from './SignUp'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <>
+    <Routes>
+         
+         <Route path="/" element={<Product/>}/>
+         <Route path="/cart" element={<Cart/>} />
+           <Route path="/Login" element={<Login />} />
+           <Route path="/signup" element={<SignUp/>}/>
+           
+          
+     </Routes>
+    
+   
+        </>
   );
 }
 
---
+++ src/Cart.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Cart.css"
+import Header from './Header'
+const Cart = () => {
+  return (
+    <>
+    <Header/>
+    <div className='cart'>
+    <div className='cart-item'>
+    Cart is Empty
+    </div>
+     
+    </div>
+    </>
+  )
+}
+
+export default Cart
